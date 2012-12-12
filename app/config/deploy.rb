5653820bce6f0e702e97acf292e8fa2ba5369558
Milos Colakovic <gmefox@gmail.com>
2012-12-12 16:38:27 +0100

    Fix range input for Firefox

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,11 +1,15 @@
+set :stage_dir, 'app/config/deploy'
+require 'capistrano/ext/multistage'
+set :stages, %w(production testing development)
+
 set :application, "Yuno"
-set :domain,      "s2-marketing.net"
-set :deploy_to,   "/home/s2market/yuno"
+#set :domain,      "s2-marketing.net"
+#set :deploy_to,   "/home/s2market/yuno"
 set :app_path,    "app"
 
 set :use_sudo,    false
-set :user,        "s2market"
-set :server_user, "s2market"
+#set :user,        "s2market"
+#set :server_user, "s2market"
 
 set :repository,  "git@github.com:Briareos/Yuno.git"
 set :scm,         :git
@@ -13,9 +17,9 @@ set :scm_passphrase, "metalfox"
 
 set :model_manager, "doctrine"
 
-role :web,        domain                         # Your HTTP server, Apache/etc
-role :app,        domain                         # This may be the same as your `Web` server
-role :db,         domain, :primary => true       # This is where Symfony2 migrations will run
+#role :web,        domain                         # Your HTTP server, Apache/etc
+#role :app,        domain                         # This may be the same as your `Web` server
+#role :db,         domain, :primary => true       # This is where Symfony2 migrations will run
 
 set  :keep_releases,  3
 
@@ -33,3 +37,4 @@ set :group_writable, false
 
 # In case of some "ppy" errors
 default_run_options[:pty] = true
+
