5bf076660f3a71cc69634bea93d50cb9ead7152f
Milos Colakovic <gmefox@gmail.com>
2012-12-21 17:18:26 +0100

    Fix regex

--- src/Yuno/MainBundle/EventListener/PageListener.php
+++ src/Yuno/MainBundle/EventListener/PageListener.php
@@ -23,8 +23,8 @@ class PageListener
         }
         if (!preg_match('{^/[a-z0-9_-]+\.(?:php|css|js)$}', $uri)
           && !preg_match('{^/images/[a-z0-9_-]+\.(?:jpg|jpeg|png|gif)$}', $uri)
-          && !preg_match('^/style/[a-z0-9_-]+\.css', $uri)
-          && !preg_match('^/js/[a-z0-9_-]+\.js', $uri)
+          && !preg_match('{^/style/[a-z0-9_-]+\.css$}', $uri)
+          && !preg_match('{^/js/[a-z0-9_-]+\.js$}', $uri)
         ) {
             return;
         }
