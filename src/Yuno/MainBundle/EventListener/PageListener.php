ac7d7a0947d2fdd406bef8309e050e8ba9f5c840
Milos Colakovic <gmefox@gmail.com>
2013-03-07 21:04:24 +0100

    Allow style or css folders to store css files.

--- src/Yuno/MainBundle/EventListener/PageListener.php
+++ src/Yuno/MainBundle/EventListener/PageListener.php
@@ -23,7 +23,7 @@ class PageListener
         }
         if (!preg_match('{^/[a-z0-9_-]+\.(?:php|html|css|js)$}i', $uri)
           && !preg_match('{^/images/[a-z0-9_/-]+\.(?:jpg|jpeg|png|gif)$}i', $uri)
-          && !preg_match('{^/style/[a-z0-9_-]+\.css$}i', $uri)
+          && !preg_match('{^/(?:style|css)/[a-z0-9_-]+\.css$}i', $uri)
           && !preg_match('{^/js/[a-z0-9_-]+\.js$}i', $uri)
           && !preg_match('{^/[a-z0-9]+\.xml$}i', $uri)
         ) {
